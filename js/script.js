760614bcf54b60520ba6d9b15b24ee21f92fc7fd
Mudassir Shah <56714398+MudassirShah@users.noreply.github.com>
2023-08-06 17:30:01 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	$('.js--section-features').waypoints(function(direction){
+		if(direction == "down") {
+				$('nav').addclass('sticky');
+		} else {
+				$('nav').removeclass('sticky');
+		}
+	});
+	
+	// var waypoints =$('#handler-first').waypoint(function(direction){
+	// 	notify('this.element.id + hit 25% from top of window')
+	// }, {
+	// 	offset: '25%'
+	// 	});
+	
+});
